0e7e695246867093d42de93da1cb748800c4be97
DieKautz <malte@diekautz.dev>
2021-11-02 19:41:05 +0100

    improved ux

--- src/main/kotlin/command/cmd/RefetchCommand.kt
+++ src/main/kotlin/command/cmd/RefetchCommand.kt
@@ -14,21 +14,23 @@ class RefetchCommand : SlashCommand("refetch", "Refetch current series data from
     override fun handle(interaction: SlashCommandInteraction) {
         log.warn("received refetch command from ${interaction.user.discriminatedName}" + if (interaction.server.isPresent) " on ${interaction.server.get().name}" else "")
 
-        interaction.respondLater().thenAccept {
-            it.setContent("Fetching...").update()
+        interaction.respondLater().thenAccept { interactionResponseUpdater ->
+            interactionResponseUpdater
+                .setContent("Fetching...")
+                .update()
 
             runCatching {
                 val url = interaction.firstOptionStringValue.get()
                 SeriesObserver.fetchSeries(url)
             }.onFailure {
                 log.error("refetch failed with ${it.message}")
-                interaction.createFollowupMessageBuilder()
-                    .setContent("*Refetch failed* `${it.message}`")
-                    .send()
+                interactionResponseUpdater
+                    .setContent("**Refetch failed**: `${it.message}`")
+                    .update()
             }.onSuccess {
-                interaction.createFollowupMessageBuilder()
-                    .setContent("Successfully updated fetch URL to Series No.${SeriesObserver.currentSeriesNum}")
-                    .send()
+                interactionResponseUpdater
+                    .setContent("Successfully loaded ${SeriesObserver.challenges.count()} challenges of Series No.${SeriesObserver.currentSeriesNum} from `${interaction.firstOptionStringValue.get()}`")
+                    .update()
             }
         }
 
