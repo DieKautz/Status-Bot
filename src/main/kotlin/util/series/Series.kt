a393399ce082929a0c5242178aa184f98e5ebcc8
DieKautz <malte@diekautz.dev>
2022-11-17 22:43:50 +0100

    Remove prizes array from parsing

--- src/main/kotlin/util/series/Series.kt
+++ src/main/kotlin/util/series/Series.kt
@@ -2,5 +2,10 @@ package util.series
 
 import kotlinx.serialization.Serializable
 
+
+/*
+    the prizes attribute is currently not parsed as kotlinx.serialization cant handle arrays of mixed types
+    val prizes: List<Int>,
+ */
 @Serializable
-data class Series(val prizes: List<Int>, val challenges: List<Challenge>)
+data class Series(val challenges: List<Challenge>)
